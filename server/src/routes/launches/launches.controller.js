0c1b16497a1b8084d1682eeb533416d19972f3e4
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-08-28 23:36:20 +0530

    controller launches changes

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -6,3 +6,34 @@ const {
 } = require("../../models/launches.model");
 
 const { getPagination } = require("../../services/query");
+
+async function httpGetAllLaunches(req, res) {
+  const { skip, limit } = getPagination(req.query);
+  const launches = await getAllLaunches(skip, limit);
+  return res.status(200).json(launches);
+}
+
+async function httpAddNewLaunch(req, res) {
+  const launch = req.body;
+
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.target
+  ) {
+    return res.status(400).json({
+      error: "Missing required launch property",
+    });
+  }
+
+  launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: "Invalid launch date",
+    });
+  }
+
+  await scheduleNewLaunch(launch);
+  return res.status(201).json(launch);
+}
