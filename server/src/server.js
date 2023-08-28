5885bbe0a109df687967176bdf58730a5747934e
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-08-28 15:27:54 +0530

    Connecting Mongo Atlast database!

--- server/src/server.js
+++ server/src/server.js
@@ -3,3 +3,6 @@ const http = require("http");
 require("dotenv").config();
 
 const app = require("./app");
+const { mongoConnect } = require("./services/mongo");
+const { loadPlanetsData } = require("./models/planets.model");
+const { loadLaunchData } = require("./models/launches.model");
---
+++ server/src/services/mongo.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+// this is the URl for the mongo atlas
+// database URL
+const MONGO_URL = process.env.MONGO_URL;
