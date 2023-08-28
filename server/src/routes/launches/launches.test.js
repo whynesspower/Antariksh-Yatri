8cc5e6ff5bb674db6b3c666a94a12883468ffe11
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-08-28 23:47:36 +0530

    Starting the tests

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,4 @@
+const request = require("supertest");
+const app = require("../../app");
+const { mongoConnect, mongoDisconnect } = require("../../services/mongo");
+const { loadPlanetsData } = require("../../models/planets.model");
