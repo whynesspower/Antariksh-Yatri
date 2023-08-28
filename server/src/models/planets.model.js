eb12d80e63b17d970dfe9562fbc310f82f34bd7d
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-08-28 15:51:57 +0530

    Adding model for the planets

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -3,3 +3,41 @@ const path = require("path");
 const { parse } = require("csv-parse");
 
 const planets = require("./planets.mongo");
+
+async function getAllPlanets() {
+  return await planets.find(
+    {},
+    {
+      _id: 0,
+      __v: 0,
+    }
+  );
+}
+
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(
+      path.join(__dirname, "..", "..", "data", "kepler_data.csv")
+    )
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", async (data) => {
+        if (isHabitablePlanet(data)) {
+          savePlanet(data);
+        }
+      })
+      .on("error", (err) => {
+        console.log(err);
+        reject(err);
+      })
+      .on("end", async () => {
+        const countPlanetsFound = (await getAllPlanets()).length;
+        console.log(`${countPlanetsFound} habitable planets found!`);
+        resolve();
+      });
+  });
+}
