e60932ae6a94f49e9ee358b6efa71cd26645f811
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-08-28 16:32:30 +0530

    Adding API endpoints

--- server/src/server.js
+++ server/src/server.js
@@ -6,7 +6,7 @@ const app = require("./app");
 const { mongoConnect } = require("./services/mongo");
 const { loadPlanetsData } = require("./models/planets.model");
 const { loadLaunchData } = require("./models/launches.model");
-const { start } = require("repl");
+// const { start } = require("repl");
 
 const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
