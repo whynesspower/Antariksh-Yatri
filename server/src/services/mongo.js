87aa24c60d0070f7553a852a3d8a99350f156462
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-08-28 15:29:48 +0530

    Fuctions for connecting and disconnecting db

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -13,3 +13,16 @@ mongoose.connection.once("open", () => {
 mongoose.connection.on("error", (err) => {
   console.error(err);
 });
+
+async function mongoConnect() {
+  await mongoose.connect(MONGO_URL);
+}
+
+async function mongoDisconnect() {
+  await mongoose.disconnect();
+}
+
+module.exports = {
+  mongoConnect,
+  mongoDisconnect,
+};
