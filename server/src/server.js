e6d076280cfc6734bbd71adef15fb4e13c90b409
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-08-28 15:34:04 +0530

    Added code for connecting server with the models

--- server/src/server.js
+++ server/src/server.js
@@ -6,3 +6,16 @@ const app = require("./app");
 const { mongoConnect } = require("./services/mongo");
 const { loadPlanetsData } = require("./models/planets.model");
 const { loadLaunchData } = require("./models/launches.model");
+
+const PORT = process.env.PORT || 8000;
+const server = http.createServer(app);
+
+async function startServer() {
+  await mongoConnect();
+  await loadPlanetsData();
+  await loadLaunchData();
+
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
+}
