bad29fa6c566f0498a67b4369cd316006d8ed4d4
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-08-28 23:36:44 +0530

    adding aborted launches controller

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -37,3 +37,25 @@ async function httpAddNewLaunch(req, res) {
   await scheduleNewLaunch(launch);
   return res.status(201).json(launch);
 }
+
+async function httpAbortLaunch(req, res) {
+  const launchId = Number(req.params.id);
+
+  const existsLaunch = await existsLaunchWithId(launchId);
+  if (!existsLaunch) {
+    return res.status(404).json({
+      error: "Launch not found",
+    });
+  }
+
+  const aborted = await abortLaunchById(launchId);
+  if (!aborted) {
+    return res.status(400).json({
+      error: "Launch not aborted",
+    });
+  }
+
+  return res.status(200).json({
+    ok: true,
+  });
+}
