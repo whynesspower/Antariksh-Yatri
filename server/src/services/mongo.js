bde39306826f373f782d595cf7881b0cab67f37e
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-08-28 15:28:30 +0530

    Initial code for database

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -5,3 +5,11 @@ require("dotenv").config();
 // this is the URl for the mongo atlas
 // database URL
 const MONGO_URL = process.env.MONGO_URL;
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
