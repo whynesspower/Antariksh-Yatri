96bb2fd756d2b7b8f22feb1b1dd68d036b176b1d
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-08-28 23:22:01 +0530

    Setting up controller

---
+++ server/src/routes/launches/launches.controller.js
@@ -0,0 +1,8 @@
+const {
+  getAllLaunches,
+  scheduleNewLaunch,
+  existsLaunchWithId,
+  abortLaunchById,
+} = require("../../models/launches.model");
+
+const { getPagination } = require("../../services/query");
