4113a4313bba1e168dd09076e7262b617094f856
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-08-28 15:57:20 +0530

    Integrating SpaceX API

---
+++ server/src/models/launches.model.js
@@ -0,0 +1,8 @@
+const axios = require("axios");
+
+const launchesDatabase = require("./launches.mongo");
+const planets = require("./planets.mongo");
+
+const DEFAULT_FLIGHT_NUMBER = 100;
+
+const SPACEX_API_URL = "https://api.spacexdata.com/v4/launches/query";
